dfb1cfffeec21c2451791bf028700b421352343a
SeoriNn <train1312@naver.com>
2022-04-14 17:36:30 +0900

    update

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,67 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv'); //ip, port store
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const morgan = require("morgan");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
 
 dotenv.config();
-const pageRouter = require('./routes/page');
+//const authRouter = require("./routes/auth");
+//const indexRouter = require("./routes");
+const { sequelize } = require("./models");
+//const passportConfig = require("./passport");
 
 const app = express();
-app.set('port', process.env.PORT || 8008);
-app.set('view engine', html);
-nunjucks.configure('views', {
-    express: app,
-    watch: true,
-});
+//passportConfig();
+app.set("port", process.env.PORT);
+app.set("view engine", "html");
+//nunjucks.config("views", {
+//    express: app,
+//    watch: true,
+//});
+sequelize.sync({ force: false })
+    .then(()=> {
+        console.log("db connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));;
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
     resave: false,
-    saveUninitialized: false,
+    saveUninitalized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-        httpOnly : true,
+        httpOnly: true,
         secure: false,
-    }
+    },
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', pageRouter);
+//app.use("/auth", authRouter);
+//app.use("/", indexRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} not router`);
+    const error = new Error(`${req.method} ${req.url} no router`);
     error.status = 404;
     next(error);
 });
 
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'prodiction' ? err : {};
+    res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
 });
 
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), 'port waiting');
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "wating this port");
 });
